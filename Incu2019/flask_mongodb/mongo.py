bd445d6a828b117239d94a18acf1e28945cad8a1
N. Alejandra Estevez <nestevez7@gmail.com>
2019-01-10 12:38:31 +0000

    created function that deletes documents matching first and last names

--- Incu2019/flask_mongodb/mongo.py
+++ Incu2019/flask_mongodb/mongo.py
@@ -18,9 +18,8 @@ def update_entry(fname, lname, newinfo):
     collection = get_mongo()
     collection.update_one({"$and":[ {"firstname": fname}, {"lastname": lname} ]}, {"$set": newinfo})
 
-#  displays documents matching both first and last names
+#  displays documents matching both first and last names (or variations of them)
 #  if None, displays all documents sorted alphabetically by last names
-
 def display_entries(fname=None, lname=None):
     collection = get_mongo()
     if fname == None and lname == None:
@@ -33,3 +32,15 @@ def display_entries(fname=None, lname=None):
         docs = collection.find({"$and": [{"firstname": {"$regex": fname}},
                                          {"lastname": {"$regex": lname}} ]
                               })
+
+#  deletes documents matching both first and last names (or variations of them)
+def delete_entries(fname=None, lname=None):
+    collection = get_mongo()
+    fname = "(?=" + fname + ")\w+"
+    lname = "(?=" + lname + ")\w+"
+    collection.delete_many({"$and": [{"firstname": {"$regex": fname}},
+                                {"lastname": {"$regex": lname}}]
+                     })
+    docs = collection.find()
+    for d in docs:
+        print(d)
