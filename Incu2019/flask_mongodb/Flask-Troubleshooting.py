2b289c89902722ef4f6755ee80b09064a663414b
N. Alejandra Estevez <nestevez7@gmail.com>
2019-01-24 05:51:40 +0000

    corrected bug in Flask-Troubleshoot.py - incomplete

---
+++ Incu2019/flask_mongodb/Flask-Troubleshooting.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import abort
+from flask import Response
+from flask import request
+import json
+
+app = Flask(__name__)
+
+global authorized_users
+authorized_users=['agata','tristan','peter','joanna']
+
+@app.route("/hello/<usrname>")
+def hello(usrname):
+    return "Hello World %s !" % usrname
+
+@app.route("/authorized_only/<usrname>")
+def list_all(usrname):
+	# global authorized_users
+	if (usrname in authorized_users):
+		abort(500,'The user %s is not authorized to view the page' % usrname)
+	else:
+		return 'Welcome to your personal page, %s ' % (usrname)
+
+app.run(port=7676)
