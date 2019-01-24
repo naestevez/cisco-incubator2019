5da7ed48c32f9fd01413f6bb9122d98f751f2066
N. Alejandra Estevez <nestevez7@gmail.com>
2019-01-24 05:50:10 +0000

    add incomplete flask-mongo app with new templates

--- Incu2019/flask_mongodb/addressbook_flask.py
+++ Incu2019/flask_mongodb/addressbook_flask.py
@@ -1,36 +1,43 @@
-from flask import Flask, request, render_template
-import uuid
-# from flask_pymongo import PyMongo
-# from mongo import *
+import addressbook_mongo
+from flask import Flask, request, render_template, json, jsonify
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
-# app.config["MONGO_DBNAME"] = get_mongo()
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/incubator"
-# mongo = PyMongo(app)
 
-entry={}
-all_entries={}
+app.config["MONGO_DBNAME"] = addressbook_mongo.get_mongo()
+app.config["MONGO_URI"] = "mongodb://localhost:27017/incubator"
+mongo = PyMongo(app)
+
 
 @app.route("/", methods=["GET", "POST"])
 def new_entry():
     if request.method == "GET":
         return render_template("add_new.html")
     else:
-        fname = request.form.get("fname")
-        lname = request.form.get("lname")
-        phone = request.form.get("phone")
-        email = request.form.get("email")
-        user_id = uuid.uuid4().hex
-        entry.clear()
-        entry["fname"] = fname
-        entry["lname"] = lname
-        entry["phone"] = phone
-        entry["email"] = email
-        all_entries[user_id] = entry
-        return render_template("view_entry.html", fname=fname, lname=lname, phone=phone, email=email)
+        entry_list = []
+        entry_dict = {
+        "fname" : request.form.get("fname"),
+        "lname" : request.form.get("lname"),
+        "phone" : request.form.get("phone"),
+        "email" : request.form.get("email")
+        }
+
+        for key,value in entry_dict.items():
+            entry_list.append({key:value})
+        entry = jsonify(json.dumps(entry_list))
+
+        # addressbook_mongo.insert_new_entry(entry_dict)
+
+        return render_template("view_entry.html", entry = entry_dict)
 
 @app.route("/view_all")
 def viewall():
+    all_entries = addressbook_mongo.display_entries()
     return render_template("view_all.html", all_entries = all_entries)
+
+@app.route("/delete_all", methods = ["DELETE"])
+def delete_all():
+    addressbook_mongo.delete_all_entries()
 
 app.run(host="0.0.0.0", port=7676)
--- Incu2019/flask_mongodb/addressbook_mongo.py
+++ Incu2019/flask_mongodb/addressbook_mongo.py
@@ -11,7 +11,13 @@ def get_mongo():
 #  creates new document based on data parameter (newdoc is a dictionary)
 def insert_new_entry(newdoc):
     collection = get_mongo()
-    doc = collection.insert_one(newdoc)
+    doc = collection.insert(newdoc)
+    return doc
+
+# def view_new_entry():
+#     collection = get_mongo()
+#     view_entry = db.collection.find().limit(1).sort({$natural: -1}).pretty()
+#     return view_entry
 
 #  updates a document matching first and last names with new data (newinfo is a dictionary)
 def update_entry(fname, lname, newinfo):
@@ -24,15 +30,14 @@ def display_entries(fname=None, lname=None):
     collection = get_mongo()
     if fname == None and lname == None:
         docs = collection.find().sort("lastname")
-        for d in docs:
-            print(d)
+        return docs
     else:
         fname = "(?=" + fname + ")\w+"
         lname = "(?=" + lname + ")\w+"
         docs = collection.find({"$and": [{"firstname": {"$regex": fname}},
                                          {"lastname": {"$regex": lname}} ]
                               })
-
+        return docs
 #  deletes documents matching both first and last names (or variations of them)
 def delete_entries(fname=None, lname=None):
     collection = get_mongo()
@@ -44,3 +49,7 @@ def delete_entries(fname=None, lname=None):
     docs = collection.find()
     for d in docs:
         print(d)
+
+def delete_all_entries():
+    collection = get_mongo()
+    collection.delete_many({})
